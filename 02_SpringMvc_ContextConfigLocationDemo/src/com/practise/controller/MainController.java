4754520b946f7e0144746397339384397e46e0d6
sony <sony@sony-VAIO>
2021-05-03 16:49:30 +0530

    Spring MVC Context Config Location Demo

---
+++ 02_SpringMvc_ContextConfigLocationDemo/src/com/practise/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.practise.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(path="/main")
+	@ResponseBody
+	public String handleMainRequest() {
+		return "Main request captured";
+	}
+	
+	@RequestMapping(path="/getView")
+	public String handleGetView() {
+		return "Demo";
+	}
+}
